c43a442276a6f19df287c971074883c2cf0c113d
Citlalli-Lira <anacitlallilira@gmail.com>
2025-06-14 20:44:13 -0600

    Add files via upload
    Tiene el insert de Agus inicial y mi buscar y editar producto

--- papeleria_app/repositorios/producto_repo.py
+++ papeleria_app/repositorios/producto_repo.py
@@ -1,3 +1,4 @@
+
 from papeleria_app.models.producto import Producto
 from papeleria_app.database.connection import get_connection
 
@@ -28,4 +29,84 @@ def insert_productos(nombre_producto, descripcion, precio_unitario_venta,
 
     finally:
         if 'conn' in locals():
+            conn.close()
+
+def buscar_productos_por_nombre(nombre_busqueda):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        query = """
+        SELECT id_producto, nombre_producto, descripcion, 
+               precio_unitario_venta, precio_unitario_compra, 
+               stock_actual, stock_minimo, id_categoria
+        FROM Productos
+        WHERE nombre_producto LIKE %s
+        """
+
+        cursor.execute(query, (f"%{nombre_busqueda}%",))
+
+        resultados = cursor.fetchall()
+
+        productos = []
+        for r in resultados:
+            producto = Producto(
+                id_producto=r[0],
+                nombre=r[1],
+                descripcion=r[2],
+                precio_venta=r[3],
+                precio_compra=r[4],
+                stock=r[5],
+                stock_min=r[6],
+                id_categoria=r[7]
+            )
+            productos.append(producto)
+
+        return productos
+
+    except Exception as e:
+        print(f"Error al buscar productos: {e}")
+        return []
+
+    finally:
+        if conn:
+            conn.close()
+
+
+def actualizar_producto(producto: Producto):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        query = """
+        UPDATE Productos 
+        SET nombre_producto = %s,
+            descripcion = %s,
+            precio_unitario_venta = %s,
+            precio_unitario_compra = %s,
+            stock_actual = %s,
+            stock_minimo = %s,
+            id_categoria = %s
+        WHERE id_producto = %s
+        """
+
+        cursor.execute(query, (
+            producto.nombre,
+            producto.descripcion,
+            producto.precio_venta,
+            producto.precio_compra,
+            producto.stock,
+            producto.stock_min,
+            producto.id_categoria,
+            producto.id_producto
+        ))
+
+        conn.commit()
+        return True, "Producto actualizado correctamente."
+
+    except Exception as e:
+        return False, f"Error al actualizar producto: {e}"
+
+    finally:
+        if conn:
             conn.close()
